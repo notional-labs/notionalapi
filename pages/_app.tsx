c109cec16eec80a990cff78b9c16003da552c13e
Tuan Pham Anh <baabeetaa@gmail.com>
2023-02-07 17:25:31 +0700

    fix `You are using legacy implementation. Please update your code: use createWrapper() and wrapper.useWrappedStore()`

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import type { AppProps } from "next/app";
 import { wrapper } from "../store/configureStore";
+import { Provider } from "react-redux";
 
-const App = ({ Component, pageProps }: AppProps) => {
+const App = ({ Component, ...rest }: AppProps) => {
+  const { store, props } = wrapper.useWrappedStore(rest);
+  const { pageProps } = props;
   return (
-    <>
+    <Provider store={store}>
       <Component {...pageProps} />
-    </>
+    </Provider>
   );
 };
 
-export default wrapper.withRedux(App);
+export default App;
