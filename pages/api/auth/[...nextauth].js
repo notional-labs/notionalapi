2a41ab84547876fccf6bc212cd5e93081b87e7e6
Tuan Pham Anh <baabeetaa@gmail.com>
2023-02-10 04:48:25 +0700

    auth with dev-db

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,53 +1,59 @@
-import NextAuth from "next-auth"
+import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
+import { PrismaClient } from '@prisma/client';
+var crypto = require('crypto')
+
+const prisma = new PrismaClient()
 
 export const authOptions = {
-    // Configure one or more authentication providers
-    providers: [
-        CredentialsProvider({
-            // The name to display on the sign in form (e.g. "Sign in with...")
-            name: "Credentials",
-            // `credentials` is used to generate a form on the sign in page.
-            // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-            // e.g. domain, username, password, 2FA token, etc.
-            // You can pass any HTML attribute to the <input> tag through the object.
-            credentials: {
-                username: { label: "Username", type: "text", placeholder: "jsmith" },
-                password: { label: "Password", type: "password" }
-            },
-            async authorize(credentials, req) {
-                // Add logic here to look up the user from the credentials supplied
-                const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      // `credentials` is used to generate a form on the sign in page.
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        email: {label: "Email", type: "text", placeholder: "user@domain.com"},
+        password: {label: "Password", type: "password"}
+      },
+      async authorize(credentials, req) {
+        // Add logic here to look up the user from the credentials supplied
+        // const user = {email: "jsmith@example.com"}
+        const user = await prisma.user.findUnique({where: {email: credentials.email}});
 
-                if (user) {
-                    // Any object returned will be saved in `user` property of the JWT
-                    return user
-                } else {
-                    // If you return null then an error will be displayed advising the user to check their details.
-                    return null
+        // compare sha256(password) and user.pass_hash
+        const input_hash = crypto.createHash('sha256').update(credentials.password).digest('hex');
 
-                    // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
-                }
-            }
-        })
-    ],
-    secret: "this is secret",
-    callbacks: {
-        async jwt({ token, account }) {
-            // Persist the OAuth access_token to the token right after signin
-            if (account) {
-                token.accessToken = account.access_token
-                // token.userRole = "user"
-            }
-            return token
-        },
-        async session({ session, token, user }) {
-            // Send properties to the client, like an access_token from a provider.
-            session.accessToken = token.accessToken
-            // session.userRole = "user"
-            return session
+        if (input_hash === user.pass_hash) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user
+        } else {
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null
+
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
         }
+      }
+    })
+  ],
+  secret: "this is secret",
+  callbacks: {
+    async jwt({token, account}) {
+      // Persist the OAuth access_token to the token right after signin
+      if (account) {
+        token.accessToken = account.access_token
+        // token.userRole = "user"
+      }
+      return token
+    },
+    async session({session, token, user}) {
+      // Send properties to the client, like an access_token from a provider.
+      session.accessToken = token.accessToken
+      // session.userRole = "user"
+      return session
     }
+  }
 }
 
 export default NextAuth(authOptions)
