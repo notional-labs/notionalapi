8fa08ddd38b96c4d33f9b340c316ecc76448089d
Tuan Pham Anh <baabeetaa@gmail.com>
2023-02-10 23:16:44 +0700

    create form UI for /create_apikey page

--- pages/create_apikey.js
+++ pages/create_apikey.js
@@ -1,5 +1,15 @@
-import { Statistic, Button, Space } from 'antd';
+import { Button, Form, Input, InputNumber } from 'antd';
 import { useSession } from "next-auth/react";
+
+/* eslint-disable no-template-curly-in-string */
+const validateMessages = {
+  required: '${label} is required!',
+};
+/* eslint-enable no-template-curly-in-string */
+
+const onFinish = (values) => {
+  console.log(values);
+};
 
 export default function CreateApiKey() {
   const {data: session, status} = useSession();
@@ -9,9 +19,12 @@ export default function CreateApiKey() {
 
   return (
     <>
-      <Space>
-        <h3>Create Api-Key</h3>
-      </Space>
+      <h3>Create Api-Key</h3>
+      <Form name="basic" onFinish={onFinish} labelCol={{span: 8}} wrapperCol={{span: 16}} style={{maxWidth: 600}}>
+        <Form.Item name="name" label="Name" rules={[{required: true}]}><Input /></Form.Item>
+        <Form.Item name="note" label="Note"><Input.TextArea /></Form.Item>
+        <Form.Item wrapperCol={{span: 16, offset: 8}}><Button type="primary" htmlType="submit">Submit</Button></Form.Item>
+      </Form>
     </>
   );
 }
