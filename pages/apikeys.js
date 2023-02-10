836741c17707d58efec0d68b42d09416275b6d82
Tuan Pham Anh <baabeetaa@gmail.com>
2023-02-10 15:33:08 +0700

    Show key (#13)
    * implement Show Key button using use Antd Modal

--- pages/apikeys.js
+++ pages/apikeys.js
@@ -1,12 +1,13 @@
-import { List, Button, Space } from 'antd';
+import { List, Button, Space, Modal } from 'antd';
 import { useSession } from "next-auth/react";
 import useSWR from 'swr';
+import { useState } from 'react';
 
 const fetcher = (...args) => fetch(...args).then(res => res.json());
 
 export default function ApiKeys() {
   const {data: session, status} = useSession()
-  const { data, error, isLoading } = useSWR('/api/my_apikeys', fetcher)
+  const { data, error, isLoading } = useSWR('/api/my_apikeys', fetcher);
 
   if (status === "loading" || isLoading) return <p>Loading...</p>
   if (status === "unauthenticated") return <p>Access Denied.</p>
@@ -23,7 +24,12 @@ export default function ApiKeys() {
         dataSource={data}
         renderItem={(item) => (
           <List.Item
-            actions={[<a key="list-view-key">Show Key</a>, <a key="list-delete-key">Delete</a>]}
+            actions={[<Button type="text" onClick={() => {
+              Modal.info({
+                title: 'Api Key',
+                content: (<>{item.apikey}</>),
+              });
+            }}>Show Key</Button>, <a key="list-delete-key">Delete</a>]}
           >
             <List.Item.Meta
               title={item.name}
