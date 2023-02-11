cbd33b36b96a629907016a46707e82548ba09683
Tuan Pham Anh <baabeetaa@gmail.com>
2023-02-11 08:00:02 +0700

    ...

--- pages/create_apikey.js
+++ pages/create_apikey.js
@@ -1,30 +1,37 @@
 import { Button, Form, Input } from 'antd';
 import { useSession } from "next-auth/react";
+import { useState } from 'react';
 
 export default function CreateApiKey() {
   const {data: session, status} = useSession();
+  const [loading, setLoading] = useState(false);
+  const [result, setResult] = useState("");
 
-  if (status === "loading") return <p>Loading...</p>
+  if (status === "loading" || loading) return <p>Loading...</p>
   if (status === "unauthenticated") return <p>Access Denied.</p>
+  if (result === "success") return <p>Success</p>
+  if (result === "failed") return <p>Failed</p>
 
   const onFinish = async (values) => {
-    const rawResponse = await fetch('/api/create_apikey', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(values),
-    });
-    const content = await rawResponse.json();
-    console.log(`content=${JSON.stringify(content)}`);
-    const {apikey} = content;
-    if (apikey) {
-      alert("created OK!");
-      return;
+    try {
+      setLoading(true);
+      const rawResponse = await fetch('/api/create_apikey', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(values),
+      });
+      const content = await rawResponse.json();
+      const {apikey} = content;
+      if (apikey) setResult("success");
+      else setResult("failed");
+    } catch (e) {
+      setResult("failed");
+    } finally {
+      setLoading(false);
     }
-
-    alert("created failed!");
   };
 
   return (
