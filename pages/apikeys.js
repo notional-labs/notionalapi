4dfcf501c600a42d182587415afb49ff59e493d3
Tuan Pham Anh <baabeetaa@gmail.com>
2023-02-11 17:46:53 +0700

    works

--- pages/apikeys.js
+++ pages/apikeys.js
@@ -1,4 +1,4 @@
-import { List, Button, Space, Modal } from 'antd';
+import { List, Button, Space, Modal, message } from 'antd';
 import { useSession } from "next-auth/react";
 import useSWR from 'swr';
 import { useState } from 'react';
@@ -10,28 +10,51 @@ const fetcher = (...args) => fetch(...args).then(res => res.json());
 
 export default function ApiKeys() {
   const {data: session, status} = useSession()
-  const { data, error, isLoading } = useSWR('/api/my_apikeys', fetcher);
+  const { data, error, isLoading, mutate } = useSWR('/api/my_apikeys', fetcher);
+  const [messageApi, contextHolder] = message.useMessage();
 
   if (status === "loading" || isLoading) return <p>Loading...</p>
   if (status === "unauthenticated") return <p>Access Denied.</p>
   if (error) return <p>failed to load</p>
 
-  const showConfirm = () => {
+  const showConfirm = (apikey) => {
     confirm({
       title: 'Confirm',
       icon: <ExclamationCircleFilled />,
       content: 'continue to delete this ApiKey?',
-      onOk() {
-        console.log('OK');
+      onOk: async () => {
+        try {
+          setLoading(true);
+
+          const rawResponse = await fetch('/api/delete_apikey', {
+            method: 'POST',
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({apikey}),
+          });
+          // const deletedApiKey = await rawResponse.json();
+
+          messageApi.open({type: 'success', content: 'ApiKey deleted!'});
+        } catch (e) {
+          messageApi.open({type: 'error', content: `something wrong (${e.message})`});
+        } finally {
+          // always refresh
+          await mutate('/api/my_apikeys');
+          setLoading(false);
+        }
+
       },
-      onCancel() {
-        console.log('Cancel');
-      },
+      // onCancel() {
+      //   console.log('Cancel');
+      // },
     });
   };
 
   return (
     <>
+      {contextHolder}
       <Space>
         <h3>Api-Keys</h3>
         <Link href='/create_apikey'><Button>New Key</Button></Link>
@@ -45,7 +68,9 @@ export default function ApiKeys() {
               <Button type="text" onClick={() => {
                 Modal.info({title: 'Api Key', content: (<>{item.apikey}</>)});
               }}>Show Key</Button>,
-              <Button type="text" onClick={showConfirm}>Delete</Button>]}
+              <Button type="text" onClick={() => {
+                showConfirm(item.apikey);
+              }}>Delete</Button>]}
           >
             <List.Item.Meta
               title={item.name}
