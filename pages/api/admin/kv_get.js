6ce2dbc3eae6778a73af928943f62370106b0ba4
Tuan Pham Anh <baabeetaa@gmail.com>
2023-02-26 00:29:32 +0700

    ...

--- pages/api/admin/kv_get.js
+++ pages/api/admin/kv_get.js
@@ -1,17 +1,5 @@
 import {checkBasicAuth} from '/helper/basic_auth';
 
-
-const isEmpty = (value) => {
-  return (value == null || value.length === 0);
-}
-
-const checkAuth = (username, password) => {
-  if (username === 'admin' && password === 'admin') {
-    return true;
-  }
-  return false;
-}
-
 export default async (req, res) => {
   // Basic Authentication
   if (checkBasicAuth({req, res}) !== true) return res.send({error: "Access Denied."});
