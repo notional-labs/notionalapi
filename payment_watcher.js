eaa5a6f49f20320ec1b44e0498989c340071a933
Tuan Pham Anh <baabeetaa@gmail.com>
2023-02-25 17:48:33 +0700

    ...

--- payment_watcher.js
+++ payment_watcher.js
@@ -1,9 +1,20 @@
+/**
+ * There are 2 ways to implement:
+ * 1) pooling to api/lcd: /cosmos/tx/v1beta1/txs
+ * 2) scan block by block
+ *
+ * pooling is easier however pagination.key, and pagination.offset dont work, so we'll implement using `scan block by block`
+ */
+
 const { IndexedTx, StargateClient } = require('@cosmjs/stargate');
 const { Tx } = require("cosmjs-types/cosmos/tx/v1beta1/tx");
 const { MsgSend } = require("cosmjs-types/cosmos/bank/v1beta1/tx");
+const { sha256 } = require("@cosmjs/crypto");
+const { toHex } = require("@cosmjs/encoding");
+const { Uint53 } = require("@cosmjs/math");
 
-
-// const FOO = 'bar';
+const WATCH_ADDR = 'osmo1038lvdayf3c96lpu05c09rf3jua2jhycvcavw5';
+const WATCH_DENOM = 'ibc/D189335C6E4A68B513C10AB227BF1C1D38C746766278BA3EEB4FB14124F1D858';
 
 
 sleep = (ms) => {
@@ -21,6 +32,10 @@ main = async () => {
 
   for (const tx of txs) {
     // console.log(tx);
+
+    const txHash = toHex(sha256(Buffer.from(tx,'base64')));
+    // console.log(txHash);
+
     const decodedTx = Tx.decode(tx);
     // console.log("DecodedTx:", decodedTx);
     const {memo, messages} = decodedTx.body;
@@ -31,16 +46,26 @@ main = async () => {
         console.log("Sent message:", sendMessage, " with memo ", memo);
         const {fromAddress, toAddress, amount} = sendMessage;
 
-        if ((toAddress == 'osmo1038lvdayf3c96lpu05c09rf3jua2jhycvcavw5') &&
-          (amount.denom === 'ibc/D189335C6E4A68B513C10AB227BF1C1D38C746766278BA3EEB4FB14124F1D858')) {
+        // {
+        //   fromAddress: 'osmo1tl9nq9e7jle92cxeas9c4qcaqnvdguwx9ue3qu',
+        //   toAddress: 'osmo1038lvdayf3c96lpu05c09rf3jua2jhycvcavw5',
+        //   amount: [
+        //     {
+        //       denom: 'ibc/D189335C6E4A68B513C10AB227BF1C1D38C746766278BA3EEB4FB14124F1D858',
+        //       amount: '3000000000'
+        //     }
+        //   ]
+        // }
 
-          // TODO: check amount here
-          // do the logic
+        const amount_number = Uint53.fromString(amount[0].amount).toNumber();
+        console.log("amount_number:", amount_number);
+
+        if ((toAddress == WATCH_ADDR) && (amount.denom === WATCH_DENOM) && (amount_number > 0)) {
+          // TODO: insert to db for processing
 
         }
       }
     }
-
   }
 
 
